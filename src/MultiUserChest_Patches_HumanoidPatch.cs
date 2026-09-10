eaf8394738bb9eaac92c9a28378e84af93ce46bf
Swmarly <69097826+Swmarly@users.noreply.github.com>
2026-09-10 19:02:22 +0200

    Finish authoritative multiplayer chest audit for 1.0.3

--- src/MultiUserChest_Patches_HumanoidPatch.cs
+++ src/MultiUserChest_Patches_HumanoidPatch.cs
@@ -5,8 +5,8 @@ namespace SwmarlyValheimQOL {
     public static class HumanoidPatch {
         [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.Awake)), HarmonyPostfix]
         public static void HumanoidAwakePatch(Humanoid __instance) {
+            if (!Plugin.IsFeatureEnabled(Plugin.MultiUserChests)) return;
             __instance.gameObject.AddComponent<HumanoidExtend>();
         }
     }
 }
-
