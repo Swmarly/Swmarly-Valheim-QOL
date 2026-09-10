5607e9e82ede3939ea2da9fd319ffccc912860b1
Swmarly <69097826+Swmarly@users.noreply.github.com>
2026-09-10 18:52:15 +0200

    Add authoritative shared-chest transaction layer

---
+++ src/MultiUserChest_ContainerExtend.cs
@@ -0,0 +1,34 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace SwmarlyValheimQOL {
+    public class ContainerExtend : MonoBehaviour {
+        private static readonly ConditionalWeakTable<Inventory, ContainerInventoryOwner> Containers = new ConditionalWeakTable<Inventory, ContainerInventoryOwner>();
+        private Inventory inventory;
+
+        private void Awake() {
+            if (!TryGetComponent(out Container container)) {
+                return;
+            }
+
+            inventory = container.GetInventory();
+
+            if (inventory == null) {
+                return;
+            }
+
+            Containers.TryAdd(inventory, i => new ContainerInventoryOwner(container));
+        }
+
+        private void OnDestroy() {
+            if (inventory != null) {
+                Containers.Remove(inventory);
+            }
+        }
+
+        public static bool GetContainer(Inventory inventory, out ContainerInventoryOwner container) {
+            return Containers.TryGetValue(inventory, out container);
+        }
+    }
+}
+
